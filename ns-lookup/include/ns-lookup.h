604db70505c2809aed2026b0f90cc8d48d0a8779
Polina Kravchenko <polina.krv8@gmail.com>
2024-09-07 12:25:33 +0300

    Add project files.

---
+++ ns-lookup/include/ns-lookup.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "stdio.h"
+#include "string.h"
+#include "winsock2.h"
+#include "windows.h"
+
+int init();
+
+void cleanup();
+
+int start(int argc, char* argv[]);
+
+int resolve_dns(const char* name);
