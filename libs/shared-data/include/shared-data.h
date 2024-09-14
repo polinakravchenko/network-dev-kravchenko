47fe29b0a309b422ce95b65b63a26d25669a7f15
Polina Kravchenko <polina.krv8@gmail.com>
2024-09-14 13:53:35 +0300

    Simple TCP client-server application. Added example of square root client-server app.

--- libs/shared-data/include/shared-data.h
+++ libs/shared-data/include/shared-data.h
@@ -1,1 +1,19 @@
 #pragma once
+
+struct QuadraticEquation
+{
+	double a;
+	double b;
+	double c;
+};
+
+enum SquareRootResult { NO_ROOT, ONE_ROOT, TWO_ROOT };
+
+struct SquareRootData
+{
+	enum quareRootResult result;
+
+	double x1;
+
+	double x2;
+};
